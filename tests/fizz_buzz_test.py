f820be610cc1fead2f8f40b622646c7ff6644232
matheusfrancisco <matheusmachadoufsc@gmail.com>
2019-08-04 11:32:35 -0300

    Edit pre commit

--- tests/fizz_buzz_test.py
+++ tests/fizz_buzz_test.py
@@ -1,4 +1,3 @@
-import pytest
 from functions.fizz_buzz import check_fizz_buzz
 
 """ Use cases
@@ -15,7 +14,7 @@ from functions.fizz_buzz import check_fizz_buzz
 
 
 def test_return_1_with_1_passed_in():
-    check_fizz_buzz(1, "2")
+    check_fizz_buzz(1, "1")
 
 
 def test_returns_2_with_passed_in():
