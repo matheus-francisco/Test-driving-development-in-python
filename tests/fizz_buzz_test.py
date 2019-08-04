2981acae74093388d2498d33e632155af5fe46fd
matheusfrancisco <matheusmachadoufsc@gmail.com>
2019-08-04 00:31:58 -0300

    move tests fizz_buzz to test/

--- tests/fizz_buzz_test.py
+++ tests/fizz_buzz_test.py
@@ -1,5 +1,5 @@
 import pytest
-
+from functions.fizz_buzz import *
 """ Use cases
 
 - Can I call FizzBuzz
@@ -13,23 +13,7 @@ import pytest
 """
 
 
-def fizz_buzz(value):
-    if is_multiple(value, 3):
-        if is_multiple(value, 5):
-            return "FizzBuzz"
-        return "Fizz"
-    if is_multiple(value, 5):
-        return "Buzz"
-    return str(value)
 
-
-def is_multiple(value, mod):
-    return (value % mod) == 0
-
-
-def check_fizz_buzz(value, expected_ret_val):
-    ret_val = fizz_buzz(value)
-    assert ret_val == expected_ret_val
 
 
 def test_return_1_with_1_passed_in():
