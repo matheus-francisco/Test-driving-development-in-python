37b509f84a9af3fc93ba23ec8a4404c5cf83604c
matheusfrancisco <matheusmachadoufsc@gmail.com>
2019-08-04 00:32:15 -0300

    move function fizz_buzz to functions/

---
+++ functions/fizz_buzz.py
@@ -0,0 +1,19 @@
+import os
+
+def fizz_buzz(value):
+    if is_multiple(value, 3):
+        if is_multiple(value, 5):
+            return "FizzBuzz"
+        return "Fizz"
+    if is_multiple(value, 5):
+        return "Buzz"
+    return str(value)
+
+
+def is_multiple(value, mod):
+    return (value % mod) == 0
+
+
+def check_fizz_buzz(value, expected_ret_val):
+    ret_val = fizz_buzz(value)
+    assert ret_val == expected_ret_val
