4d6766a89c28d4161d87c5356c776904da39e7a5
matheusfrancisco <matheusmachadoufsc@gmail.com>
2019-08-04 21:32:24 -0300

    add example with fixtures

--- tests/test_fixtures.py
+++ tests/test_fixtures.py
@@ -2,8 +2,24 @@ import pytest
 
 
 @pytest.fixture()
-def setup():
-    print("\nSetup")
+def setup1():
+    print("\nSetup 1")
+    yield
+    print("\nTeardown 1")
+
+
+@pytest.fixture()
+def setup2(request):
+    print("\n Setup 2")
+
+    def teardown_a():
+        print("\n Teardown A")
+
+    def teardown_b():
+        print("\n Teardown B")
+
+    request.addfinalizer(teardown_a)
+    request.addfinalizer(teardown_b)
 
 
 def test_1(setup):
