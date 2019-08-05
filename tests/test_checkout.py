a5840438592a7876e066c6b01068a63d207d2048
matheusfrancisco <matheusmachadoufsc@gmail.com>
2019-08-04 22:00:15 -0300

    refactoring code and create new test

--- tests/test_checkout.py
+++ tests/test_checkout.py
@@ -3,7 +3,7 @@ x Can create instance of Checkout class
 x Can add item price
 TODO
 
-- Can add an item
+x Can add an item
 - Can calculate the current total
 - Can add multiple items and get correct total
 - Can add discount rules
@@ -11,14 +11,25 @@ TODO
 - Can apply discount rules to the total
 - Exception is throw for item added without a price
 """
+import pytest
 from functions.checkout import Checkout
 
 
-def test_can_add_item_price():
-    co = Checkout()
-    co.add_item_price("a", 1)
+@pytest.fixtures()
+def checkout():
+    checkout = Checkout()
+    return checkout
 
 
-def test_can_add_item():
-    co = Checkout()
-    co.add_item("a")
+def test_can_add_item_price(checkout):
+    checkout.add_item_price("a", 1)
+
+
+def test_can_add_item(checkout):
+    checkout.add_item("a")
+
+
+def test_can_calculate_total(checkout):
+    checkout.add_item_price("a", 1)
+    checkout.add_item("a")
+    assert checkout.calculate_total() == 1
