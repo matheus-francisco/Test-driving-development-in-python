feb0b975de6a4ec3b9db3f3afc58e7bdae0d5401
matheusfrancisco <matheusmachadoufsc@gmail.com>
2019-08-04 22:16:56 -0300

    refactoring code

--- functions/checkout.py
+++ functions/checkout.py
@@ -1,16 +1,29 @@
 class Checkout:
+    class Discount:
+        def __init__(self, nbr_items, price):
+            self.nbr_items = nbr_items
+            self.price = price
+
     def __init__(self):
         self.prices = {}
-        self.total = 0
+        self.discounts = {}
+        self.items = {}
 
     def add_item_price(self, item, price):
         self.prices[item] = price
 
     def add_item(self, item):
-        self.total += self.prices[item]
+        if item in self.items:
+            self.items[item] += 1
+        else:
+            self.items[item] = 1
 
     def calculate_total(self):
-        return self.total
+        total = 0
+        for item, cnt in self.items.items():
+            total += self.prices[item] * cnt
+        return total
 
     def add_discount(self, item, nbr_of_items, price):
-        pass
+        discount = self.Discount(nbr_of_items, price)
+        self.discounts[item] = discount
